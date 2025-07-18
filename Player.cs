6279b50afc16c3e9640478e4fa9e155b42243c84
SheepYhangCN <sheepyuhang@163.com>
2025-07-18 21:30:09 +0800

    optimize moving in air

--- Player.cs
+++ Player.cs
@@ -11,6 +11,7 @@ public partial class Player : CharacterBody3D
 	Vector3 original_position = Vector3.Zero;
 	Vector3 original_rotation = Vector3.Zero;
 	Vector3 last_rotation = Vector3.Zero;
+	Vector3 air_velocity = Vector3.Zero;
 
 	float gravity = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
 
@@ -63,6 +64,7 @@ public partial class Player : CharacterBody3D
 			"\nFlashlight.LightEnergy: " + light.LightEnergy.ToString() + 
 			"\nPlayer.Position"+Position.ToString() + 
 			"\nPlayer.Velocity"+Velocity.ToString() + 
+			"\nPlayer.AirVelocity"+air_velocity.ToString() + 
 			"\nPlayer.RotationDegrees"+RotationDegrees.ToString() + 
 			"\nCameraTPS.Position"+CameraTPS.Position.ToString() + 
 			"\nCameraTPS.RotationDegrees"+Camera.RotationDegrees.ToString() + 
@@ -144,17 +146,43 @@ public partial class Player : CharacterBody3D
 		{
 			velocity.X = direction.X * Speed;
 			velocity.Z = direction.Z * Speed;
+			air_velocity = velocity;
 			last_rotation = Rotation;
 		}
 		else
 		{
 			if (last_rotation != Rotation)
 			{
-				Velocity = Velocity.Rotated(Vector3.Up,Rotation.Y - last_rotation.Y);
+				Velocity = Velocity.Rotated(Vector3.Up, Rotation.Y - last_rotation.Y);
 				last_rotation = Rotation;
 			}
 			velocity.X = Mathf.MoveToward(Velocity.X, 0, IsOnFloorOrGravityDisabled ? Speed / 10 : 0);
 			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, IsOnFloorOrGravityDisabled ? Speed / 10 : 0);
+			if (direction != Vector3.Zero)
+			{
+				velocity.X += direction.X * Speed / 20;
+				velocity.Z += direction.Z * Speed / 20;
+				if (air_velocity.X > 0)
+				{
+					velocity.X = Mathf.Clamp(velocity.X, -air_velocity.X, air_velocity.X);
+				}
+				else
+				{
+					velocity.X = Mathf.Clamp(velocity.X, air_velocity.X, -air_velocity.X);
+				}
+				if (air_velocity.Z > 0)
+				{
+					velocity.Z = Mathf.Clamp(velocity.Z, -air_velocity.Z, air_velocity.Z);
+				}
+				else
+				{
+					velocity.Z = Mathf.Clamp(velocity.Z, air_velocity.Z, -air_velocity.Z);
+				}
+			}
+			else if (velocity.X == 0 && velocity.Z == 0 && IsOnFloor())
+			{
+				air_velocity = Vector3.Zero;
+			}
 		}
 		Velocity = velocity;
 		MoveAndSlide();
